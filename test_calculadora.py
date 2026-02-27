8f02ae34adc36aaedf6392c6433043b4d8651c48
Jorge Santiago Guerrero Osuna <jorge.guerrero.osuna@uabc.edu.mx>
2026-02-26 19:24:36 -0800

    Metodo test_subtract
    metodo para probar subtract en calculadora

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,6 +4,9 @@ def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
 
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(5, 2) == 3
 
 #python -m pytest
 #python -m pytest -v
